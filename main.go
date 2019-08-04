a6405f6474b77233b016ba76f4b93b58a9fd2ae9
Zachary Stewart <zachary@stewarts.net>
2019-08-04 12:32:43 -0400

    Update main.go to demonstrate use of the API

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/ztstewart/subwayclock/internal/client"
+)
+
+// An example stop ID for testing purposes.
+const _courtSquareStopID = "719"
 
 func main() {
-    fmt.Println("hello")
+	nycta, _ := client.NewNYCTA(&client.Config{
+		APIKey: "",
+		FeedID: 51,
+	})
+
+	update, err := nycta.GetFeed()
+	if err != nil {
+		fmt.Println("err: " + err.Error())
+		return
+	}
+
+	// Print out a JSON formatted example of our update.
+	if status, ok := update.StationStatus[_courtSquareStopID]; ok {
+		bytes, _ := json.Marshal(status)
+		fmt.Println(string(bytes))
+	}
+
 }
